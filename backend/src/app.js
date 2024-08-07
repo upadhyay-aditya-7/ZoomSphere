5739dc6bdef56c7297c6a89a93c9edba77c73395
Aditya Upadhyay <adityaupadhyay727@gmail.com>
2024-08-07 23:50:05 +0530

    Initializing DB and Backend and Creating User Model and Creating Login/Signup Function

---
+++ backend/src/app.js
@@ -0,0 +1,35 @@
+import express from "express";
+import { createServer, ServerResponse } from "node:http";
+
+// import { Server } from "socket.io";
+
+import mongoose from "mongoose";
+import { connectToSocket } from "./controllers/socketManager.js";
+
+import cors from "cors";
+import userRoutes from "./routes/users.route.js";
+
+const app = express();
+const server = createServer(app);
+const io = connectToSocket(server);
+
+app.set("port", process.env.PORT || 8000);
+app.use(cors());
+app.use(express.json({ limit: "40kb" }));
+app.use(express.urlencoded({ limit: "40kb", extended: true }));
+
+app.use("/api/v1/users", userRoutes);
+ 
+
+const start = async () => {
+  const connectionDb = await mongoose.connect(
+    "mongodb+srv://adityaupadhyay727:obiGZ7Nv8MjwCFCo@cluster0.4wf6w.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  );
+  console.log(`MONGO Connected DB Host: ${connectionDb.connection.host}`);
+
+  server.listen(app.get("port"), () => {
+    console.log("LISTENING ON PORT 8000");
+  });
+};
+
+start();
